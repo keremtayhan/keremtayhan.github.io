2f8c856813e2b45f6108092dcff9f4397f24505a
1c3_p1ck <tayhanek@gmail.com>
2026-03-19 19:43:47 +0300

    refactor: update track details and embed links; enhance titles and durations for better clarity

--- music.js
+++ music.js
@@ -3,7 +3,7 @@ const tracks = [
     title: "MACHINA",
     caption: "saint nothing / selected transmission",
     soundcloudPage: "https://soundcloud.com/saint0nothing/machina",
-    youtube: "",
+    youtube: "https://www.youtube.com/watch?v=1enq-76VskI",
     embed: `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2209533290&color=%236984c9&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/saint0nothing" title="Saint Nothing" target="_blank" style="color: #cccccc; text-decoration: none;">Saint Nothing</a> · <a href="https://soundcloud.com/saint0nothing/machina" title="machina" target="_blank" style="color: #cccccc; text-decoration: none;">machina</a></div>`
   },
   {
@@ -14,11 +14,11 @@ const tracks = [
     embed: `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2200990379&color=%236984c9&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/saint0nothing" title="Saint Nothing" target="_blank" style="color: #cccccc; text-decoration: none;">Saint Nothing</a> · <a href="https://soundcloud.com/saint0nothing/trotting-better" title="trotting better" target="_blank" style="color: #cccccc; text-decoration: none;">trotting better</a></div>`
   },
   {
-    title: "OPEN CASKET",
+    title: "Saint Valentine",
     caption: "saint nothing / archive entry",
-    soundcloudPage: "https://soundcloud.com/your-link-here",
-    youtube: "",
-    embed: `PASTE_SOUND_CLOUD_IFRAME_HERE`
+    soundcloudPage: "https://soundcloud.com/saint0nothing/saint-valentine?si=e3e4564d18884934b38315e3fb68d623&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
+    youtube: "https://www.youtube.com/watch?v=NdL5xrUGPbY",
+    embed: `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2162793312&color=%236984c9&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/saint0nothing" title="Saint Nothing" target="_blank" style="color: #cccccc; text-decoration: none;">Saint Nothing</a> · <a href="https://soundcloud.com/saint0nothing/saint-valentine" title="Saint Valentine" target="_blank" style="color: #cccccc; text-decoration: none;">Saint Valentine</a></div>`
   }
 ];
 
