bc0121275e4ec790c74836b898603a08c0bb4cb1
1c3_p1ck <tayhanek@gmail.com>
2026-03-19 19:36:37 +0300

    refactor: update track details for "trotting better"; enhance SoundCloud embed and link

--- music.js
+++ music.js
@@ -14,11 +14,11 @@ const tracks = [
     </iframe>`
   },
   {
-    title: "CRIMINAL",
+    title: "trotting better",
     caption: "saint nothing / archive entry",
-    soundcloudPage: "https://soundcloud.com/your-link-here",
+    soundcloudPage: "https://soundcloud.com/saint0nothing/trotting-better?si=4b6f910ead6048d1be2f1e868ce2bf50&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing",
     youtube: "",
-    embed: `PASTE_SOUND_CLOUD_IFRAME_HERE`
+    embed: `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2200990379&color=%236984c9&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/saint0nothing" title="Saint Nothing" target="_blank" style="color: #cccccc; text-decoration: none;">Saint Nothing</a> · <a href="https://soundcloud.com/saint0nothing/trotting-better" title="trotting better" target="_blank" style="color: #cccccc; text-decoration: none;">trotting better</a></div>`
   },
   {
     title: "OPEN CASKET",
