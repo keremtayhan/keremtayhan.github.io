1ed5c1effbd4721225c6bc65d1fc426e06c127fd
1c3_p1ck <tayhanek@gmail.com>
2026-03-19 19:38:10 +0300

    refactor: enhance SoundCloud embed for track; update styles and functionality

--- music.js
+++ music.js
@@ -4,14 +4,7 @@ const tracks = [
     caption: "saint nothing / selected transmission",
     soundcloudPage: "https://soundcloud.com/saint0nothing/machina",
     youtube: "",
-    embed: `<iframe
-      width="100%"
-      height="166"
-      scrolling="no"
-      frameborder="no"
-      allow="autoplay"
-      src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2209533290&color=%232f5cff&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&show_playcount=false&show_artwork=true&sharing=false&download=false&buying=false">
-    </iframe>`
+    embed: `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2209533290&color=%236984c9&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/saint0nothing" title="Saint Nothing" target="_blank" style="color: #cccccc; text-decoration: none;">Saint Nothing</a> · <a href="https://soundcloud.com/saint0nothing/machina" title="machina" target="_blank" style="color: #cccccc; text-decoration: none;">machina</a></div>`
   },
   {
     title: "trotting better",
